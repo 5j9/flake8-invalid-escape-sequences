0408120bf6068dfeac9371216afd2ea9f4fb5320
5j9 <5j9@users.noreply.github.com>
2017-12-10 16:15:54 +0330

    Use regex to detect the invalid sequence
    The previous method did not work becuase Python 2's interpretation of
    raw-strings is different from python 3's.

--- flake8_invalid_escape_sequences/__init__.py
+++ flake8_invalid_escape_sequences/__init__.py
@@ -3,12 +3,14 @@
 __version__ = '0.1.3'
 
 
-from ast import literal_eval
 from re import compile as re_compile
 from token import STRING
 
 
 first_quote_search = re_compile(r'[\'"]').search
+invalid_escape_sequence_match = re_compile(
+    r'[ufb]?[\'"].*?(\\[^\\\'"abfnrtv0-7xNuU])'
+).match
 
 
 # noinspection PyUnusedLocal
@@ -17,19 +19,15 @@ def plugin(logical_line, tokens):
     for token in tokens:
         if token[0] != STRING:  # token[0] == token.type
             continue
-        string = token[1]    # token[1] == token.string
-        if '\\' not in string:
-            continue
-        quote_start = first_quote_search(string).start()
-        if 'r' in string[:quote_start]:
-            continue
-        quoted = string[quote_start:]
-        if literal_eval(quoted) == literal_eval('r' + quoted):
+        # token[1] == token.string # python 3
+        invalid_sequence_match = invalid_escape_sequence_match(token[1])
+        if invalid_sequence_match:
             yield (
                 token[2][1],  # token[2][1] == token.start.end
                 'IES: '
-                'invalid escape sequence in literal bytes or string',
+                'invalid escape sequence %s' % invalid_sequence_match.group(1),
             )
+            return
 
 
 plugin.name = 'flake8_invalid_escape_sequences'
