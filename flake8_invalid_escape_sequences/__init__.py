73015258b83f6e23b8591307e7bb3cb203d10b85
5j9 <5j9@users.noreply.github.com>
2017-12-10 12:31:32 +0330

    Flat is better than nested, remove from __future__ import unicode_literals
    Going towards py2 compatibility.

--- flake8_invalid_escape_sequences/__init__.py
+++ flake8_invalid_escape_sequences/__init__.py
@@ -1,7 +1,7 @@
 """Define a flake8 plugin to check for invalid escape sequences."""
-from __future__ import unicode_literals, absolute_import
 
 __version__ = '0.1.2'
+
 
 from ast import literal_eval
 from re import compile as re_compile
@@ -15,20 +15,21 @@ first_quote_search = re_compile(r'[\'"]').search
 def plugin(logical_line, tokens):
     """Walk the tree and detect invalid escape sequences."""
     for token in tokens:
-        if token[0] == STRING:  # token[0] == token.type
-            string = token[1]    # token[1] == token.string
-            if '\\' not in string:
-                continue
-            quote_start = first_quote_search(string).start()
-            if 'r' in string[:quote_start]:
-                continue
-            quoted = string[quote_start:]
-            if literal_eval(quoted) == literal_eval('r' + quoted):
-                yield (
-                    token[2][1],  # token[2][1] == token.start.end
-                    'IES: '
-                    'invalid escape sequence in literal bytes or string',
-                )
+        if token[0] != STRING:  # token[0] == token.type
+            continue
+        string = token[1]    # token[1] == token.string
+        if '\\' not in string:
+            continue
+        quote_start = first_quote_search(string).start()
+        if 'r' in string[:quote_start]:
+            continue
+        quoted = string[quote_start:]
+        if literal_eval(quoted) == literal_eval('r' + quoted):
+            yield (
+                token[2][1],  # token[2][1] == token.start.end
+                'IES: '
+                'invalid escape sequence in literal bytes or string',
+            )
 
 
 plugin.name = 'flake8_invalid_escape_sequences'
