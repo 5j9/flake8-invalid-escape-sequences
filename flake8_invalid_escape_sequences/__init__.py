5bf9d1a8adfe3279afa22ed43d613b5cf0b00691
5j9 <5j9@users.noreply.github.com>
2017-12-10 18:02:14 +0330

    Use tree and file_tokens instead of logical_line and tokens
    Hopefully easier to work with.
    Improve the regex.

--- flake8_invalid_escape_sequences/__init__.py
+++ flake8_invalid_escape_sequences/__init__.py
@@ -3,31 +3,41 @@
 __version__ = '0.1.3'
 
 
-from re import compile as re_compile
+from re import compile as re_compile, VERBOSE
 from token import STRING
 
 
 first_quote_search = re_compile(r'[\'"]').search
 invalid_escape_sequence_match = re_compile(
-    r'[ufb]?[\'"].*?(\\[^\\\'"abfnrtv0-7xNuU])'
+    r'''
+        [ufb]?
+        [\'"]
+        # a valid escape sequence or other character
+        (?:\\[\\\n\'"abfnrtv0-7xNuU]|[^\\])*
+        # invalid escape sequence
+        (\\[^\\\n\'"abfnrtv0-7xNuU])
+    ''',
+    VERBOSE,
 ).match
 
 
 # noinspection PyUnusedLocal
-def plugin(logical_line, tokens):
+def plugin(tree, file_tokens):
     """Walk the tree and detect invalid escape sequences."""
-    for token in tokens:
+    for token in file_tokens:
         if token[0] != STRING:  # token[0] == token.type
             continue
         # token[1] == token.string # python 3
         invalid_sequence_match = invalid_escape_sequence_match(token[1])
         if invalid_sequence_match:
             yield (
-                token[2][1],  # token[2][1] == token.start.end
-                'IES: '
-                'invalid escape sequence %s' % invalid_sequence_match.group(1),
+                token[2][0],  # line_number
+                token[2][1],  # offset
+                'IES: invalid escape sequence %s' %
+                invalid_sequence_match.group(1),  # text
+                None  # check # unused
             )
-            return
+
 
 
 plugin.name = 'flake8_invalid_escape_sequences'
