71da133b38d133a48b807ced9a7ec1d718fd9ca5
5j9 <5j9@users.noreply.github.com>
2017-12-10 11:55:32 +0330

    Wait for a key press before exiting

--- dev/pypi_upload.sh
+++ dev/pypi_upload.sh
@@ -2,4 +2,5 @@ cd ..
 python setup.py sdist
 python setup.py bdist_wheel --universal
 twine upload dist/*
+rm -r dist build flake8_invalid_escape_sequences.egg-info
 read -n1 -r -p "Press any key to continue..." key
