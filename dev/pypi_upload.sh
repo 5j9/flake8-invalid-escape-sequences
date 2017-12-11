1dae35d29c67f846df6398aaddb370d5d0cd1293
5j9 <5j9@users.noreply.github.com>
2017-12-11 11:53:33 +0330

    Do not remove egg-info

--- dev/pypi_upload.sh
+++ dev/pypi_upload.sh
@@ -2,5 +2,5 @@ cd ..
 python setup.py sdist
 python setup.py bdist_wheel --universal
 twine upload dist/*
-rm -r dist build flake8_invalid_escape_sequences.egg-info
+rm -r dist build
 read -n1 -r -p "Press any key to continue..." key
